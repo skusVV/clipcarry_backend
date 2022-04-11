f984ee107998e652434fe4f13d3aaa56db89cb2a
vskus <vskus@softserveinc.com>
2022-04-11 13:26:00 +0300

    implement template endpoints

--- src/controllers/template.controller.ts
+++ src/controllers/template.controller.ts
@@ -1,12 +1,25 @@
 import { Response, Request } from 'express';
+import { Template } from '../models/template.model';
 
 export class TemplateController {
 
     async createTemplate(req: Request, res: Response): Promise<any> {
-        return res.send({ createTemplate: 'ok' });
+        const { templateName, includeDate, includeUrl, fields } = req.body;
+        const template = new Template({
+            template_name: templateName,
+            include_date: includeDate,
+            include_url: includeUrl,
+            user_id: (req as any).user.user_id,
+            fields });
+
+        await template.save()
+
+        return res.send(template);
     }
 
     async getUserTemplates(req: Request, res: Response): Promise<any> {
-        return res.send({ getUserTemplates: 'ok' });
+        const templates = await Template.find({user_id: (req as any).user.user_id});
+
+        return res.send(templates || []);
     }
 }
