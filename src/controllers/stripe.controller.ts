abadd4eb8e048761e91677b3c857e191ee94b99b
vskus <vskus@softserveinc.com>
2022-07-01 18:32:00 +0300

    subscription beta

--- src/controllers/stripe.controller.ts
+++ src/controllers/stripe.controller.ts
@@ -21,4 +21,44 @@ export class StripeController {
       });
     }
 
+    async createPayment(req: any, res: any) {
+      const paymentLink = await stripe.paymentLinks.create({
+        line_items: [
+            {price: 'price_1LGl8QGc8jl8Z4Cc7tHGq6ec', quantity: 1}
+        ],
+        after_completion: {type: 'redirect', redirect: {url: 'http://localhost:3000/payment-success?session_id={CHECKOUT_SESSION_ID}'}},
+      });
+
+      res.send({
+        paymentLink: {
+          ...paymentLink,
+          url: paymentLink.url + '?prefilled_email=skus000@gmail.com'
+        }
+      });
+    }
+
+  async getUser(req: any, res: any) {
+      const sessionId = 'cs_test_a174htDOMUffd6JnVKMt9mMw8MDuTQkXcjzCgxmshNLLvKvB76wxjiMGY0';
+
+    const session = await stripe.checkout.sessions.retrieve(
+        sessionId
+    );
+    //     "customer": "cus_LyitSYfO5mXTja",
+    res.send({
+      session
+    });
+  }
+
+  async getPortal(req: any, res: any) {
+    const portal = await stripe.billingPortal.sessions.create({
+      customer: 'cus_LyjCvgNL33pffr',
+      return_url: 'http://localhost:3000/',
+    });
+
+    res.send({
+      portal
+    });
+  }
 }
+
+
--- src/routes/stripe.ts
+++ src/routes/stripe.ts
@@ -8,4 +8,16 @@ export const stripeRoutes = (app: any) => {
     app.post('/api/create-payment-intent', auth, (req: Request, res: Response) => {
         return stripeController.createPaymentIntent(req, res)
     });
+
+    app.get('/api/create-payment', (req: Request, res: Response) => {
+        return stripeController.createPayment(req, res)
+    });
+
+    app.get('/api/getUser', (req: Request, res: Response) => {
+        return stripeController.getUser(req, res)
+    });
+
+    app.get('/api/getPortal', (req: Request, res: Response) => {
+        return stripeController.getPortal(req, res)
+    });
 }
